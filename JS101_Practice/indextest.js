a459c8ac1c084b9df16e14f46725bc92a5373b3b
enter3017sky <enter3017sky@gmail.com>
2018-09-17 00:11:39 +0800

    update test

--- JS101_Practice/indextest.js
+++ JS101_Practice/indextest.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+
+function star(n) {
+  result = '';
+  for(let i = 1; i <= n; i++){
+    for(let j = 1; j <= i; j++){
+    }
+    result += '*'
+    console.log(result)
+  }
+  for (let i = 1; i >= n; i--) {
+    console.log(i)
+  }
+}
+star(5)
