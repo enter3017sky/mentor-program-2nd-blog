78bdf95a0c776befd1e5c704ea754156120c8b68
enter3017sky <enter3017sky@gmail.com>
2018-09-17 16:16:30 +0800

    update test5

--- JS101_Practice/indextest.js
+++ JS101_Practice/indextest.js
@@ -3,24 +3,43 @@ function star(n) {
   result = '';
   for(let i = 1; i <= n; i++){
     for(let j = 1; j <= i; j++){
+    // console.log(i)
+    // console.log(j)
+    result = '*'
+    return resule
     }
-    result += '*'
-    console.log(result)
   }
-  for (let i = 1; i >= n; i--) {
-    console.log(i)
+  for (var i = n-1; i > 0; i--) {
+    for(let j = 1; j <= i; j++){
+      // return '*'
+
+    }
   }
 }
-star(5)
+// star(5)
+console.log(star(5))
 
 
-check 1
 
 
-check 2
+    // console.log(i)
 
 
-check 3 
+
+// for(let i = 1; i <= n; i++){
+//   for(let j = 1; j <= i; j++){
+//   }
+//   result += '*'
+//   console.log(result)
+// }
+
+// check 1
 
 
-test 3 
+// check 2
+
+
+// check 3 
+
+
+// test 3 
